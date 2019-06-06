66df8164ff298f15a947ff20b8265cfacbe001a4
piladzisi <piladzisi@gmail.com>
2019-06-06 19:28:08 +0300

    spinner

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '5d3c281f8b7ece9515e8f1dac20c6b1b';
 
@@ -28,10 +30,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(url);
     var weatherData = await networkHelper.getData();
     print(weatherData);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: SpinKitCircle(
+        color: Colors.white,
+        size: 70.0,
+      ),
+    ));
   }
 }
